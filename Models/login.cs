b8980c87de0f1d863134adb3e65599fc8af10f6e
Kamohelo <Kamohelo@DESKTOP-O96A23N>
2025-10-23 23:55:01 +0200

    added comments on the  login and the error/viewModel

--- Models/login.cs
+++ Models/login.cs
@@ -4,14 +4,14 @@ namespace part_1.Models
 {
     public class login
     {
-
+        //property for  email 
 
         [Required]
         public string email { get; set; }
-
+        //propert for  password 
         [Required]
         public string password {  get; set; }
-
+        //property for  role 
         [Required]
         public string role { get; set; }
 
